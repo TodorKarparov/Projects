4438222cf5a89117a3f995239dee5add0a150683
TodorKarpaorv <tkarparow@gmail.com>
2020-10-19 13:32:45 +0300

    19.10.2020

---
+++ Files/QuizMaker/Quiz.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tkk on 19/10/2020.
+//
+
+#include "Quiz.h"
---
+++ Files/QuizMaker/Quiz.h
@@ -0,0 +1,14 @@
+//
+// Created by tkk on 19/10/2020.
+//
+
+#ifndef QUIZMAKER_QUIZ_H
+#define QUIZMAKER_QUIZ_H
+
+
+class Quiz {
+
+};
+
+
+#endif //QUIZMAKER_QUIZ_H
